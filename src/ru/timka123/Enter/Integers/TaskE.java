2ae4c905026259b1fbf220d77f3a8755209821d3
timka123 <jungle8888@mail.ru>
2022-04-19 17:40:22 +0300

    полностью сделал задачи из раздела "Введение"

--- src/ru/timka123/Enter/Integers/TaskE.java
+++ src/ru/timka123/Enter/Integers/TaskE.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class TaskE {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number % 10 == 0) {
+            System.out.println(number / 10);
+        } else {
+            System.out.println(number / 10 + 1);
+        }
+    }
 }
--- src/ru/timka123/Enter/Integers/TaskG.java
+++ src/ru/timka123/Enter/Integers/TaskG.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class TaskG {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+ 
+        int first = k / n;
+        int second = k % n;
+        if (second > 0) {
+            System.out.println(first + " " + second + " " + (n - second));
+        } else {
+            System.out.println(first + " " + second + " " + 0);
+        }
+    }
 }
